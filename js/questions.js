215ce335fa99bab048b273ed65139e92a2dc64b4
thesid77 <77628836+thesid77@users.noreply.github.com>
2021-08-02 00:01:58 +0530

    initial commit

---
+++ js/questions.js
@@ -0,0 +1,80 @@
+let MCQS = [{
+    question: "Which is the largest ocean in the world?",
+    choice1: "Indian Ocean",
+    choice2: "Pacific Ocean",
+    choice3: "Artic Ocean",
+    choice4: "Atlantic Ocean",
+    answer: 1
+},
+            {
+            question: "Which is longest river in the world?",
+    choice1: "Ganga",
+    choice2: "Nile",
+    choice3: "Amazon",
+    choice4: "Niger",
+    answer: 1
+}, 
+            {
+            question: "Which bank is called bankers bank of India?",
+    choice1: "Reserve Bank of India",
+    choice2: "Punjab National Bank",
+    choice3: "State Bank of India",
+    choice4: "ICICI Bank",
+    answer: 0
+},
+            {
+            question: "Which is largest animal on land?",
+    choice1: "Tiger",
+    choice2: "White Elephant",
+    choice3: "African Elephant",
+    choice4: "Crocodile",
+    answer: 2    
+},
+            {
+            question: "Which is the 29th state of India?",
+    choice1: "Daman and Diu",
+    choice2: "Mizoram",
+    choice3: "Telangana",
+    choice4: "Manipur",
+    answer: 2
+},
+            {
+            question: "LED stands for what?",
+    choice1: "Light emitting Device",
+    choice2: "Low emitting Diode",
+    choice3: "Light electronic Diode",
+    choice4: "Light Emitting Diode",
+    answer: 3    
+},
+            {
+               question: "Tsunami is a word in which language?",
+    choice1: "Hindi",
+    choice2: "Urdu",
+    choice3: "Japanese",
+    choice4: "French",
+    answer: 2
+},{
+               question: "Which country is the first to make Facebook and Google pay its media outlets for news content?",
+    choice1: "China",
+    choice2: "Germany",
+    choice3: "Russia",
+    choice4: "Australia",
+    answer: 3
+},
+            {
+               question: "Who is the author of book Atomic Habits?",
+    choice1: "YN Harari",
+    choice2: "Mark Manson",
+    choice3: "James Clear",
+    choice4: "Jay Shetty",
+    answer: 2
+},
+            {
+               question: "Until 1896, which country was the only source of diamonds in the world?",
+    choice1: "Russia",
+    choice2: "India",
+    choice3: "Canada",
+    choice4: "Australia",
+    answer: 1
+    
+}];
